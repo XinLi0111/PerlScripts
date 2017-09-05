0bb44604a717ad7dc409f6414e0d2ad0749271c5
Eric Lee <31495943+lx1140114195@users.noreply.github.com>
2017-09-05 15:43:16 +0800

    update procedure

--- ExtractGenbankTaxonInfo.pl
+++ ExtractGenbankTaxonInfo.pl
@@ -4,8 +4,10 @@ if (@ARGV==undef) {
 	print "Please input genbank file!\n";
 }elsif ($ARGV[0]=="-d") {
 	$gbFile = "sequence.gb";
+	print "Processing, please wait...\n";
 }else {
 	$gbFile = $ARGV[0];
+	print "Processing, please wait...\n";
 }
 
 
@@ -18,10 +20,11 @@ print TI "Accession\tSpeciesName\tTaxon\n";
 for $seqInfo (@seqInfo=<GB>){ 
 	    $accession = $seqInfo =~ s/.+VERSION\s+([A-Z]{2}_?[0-9.]+).+/$1/sr;
 	    $taxonInfo = $seqInfo =~ s/.+ORGANISM(.+?)REFERENCE.+/$1/rs;
-	    $taxonInfo =~ s/\n|\s+/_/g;
-	    $taxonInfo =~ s/_+/_/g;
-	    $taxonInfo =~ s/;_/>>/g;
-	    $taxonInfo =~ s/_([A-Z][a-z])/\t$1/g;
+	    $taxonInfo =~ s/\n/>/g;
+      $taxonInfo =~ s/\s+/_/g;
+	    $taxonInfo =~ s/\>\>/;/g;
+	    $taxonInfo =~ s/;(_|>)/>/g;
+	    $taxonInfo =~ s/_([A-Z][a-z])/$1/g;
 	    
-	    print TI "$accession$taxonInfo\n";
+	    print TI "$accession\t$taxonInfo\n";
 }
