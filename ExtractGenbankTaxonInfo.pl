2505691a873a5bbfcfe6f0c1c96d1cf850d835d3
Eric Lee <31495943+lx1140114195@users.noreply.github.com>
2017-09-05 16:03:41 +0800

    update output style

--- ExtractGenbankTaxonInfo.pl
+++ ExtractGenbankTaxonInfo.pl
@@ -20,11 +20,15 @@ print TI "Accession\tSpeciesName\tTaxon\n";
 for $seqInfo (@seqInfo=<GB>){ 
 	    $accession = $seqInfo =~ s/.+VERSION\s+([A-Z]{2}_?[0-9.]+).+/$1/sr;
 	    $taxonInfo = $seqInfo =~ s/.+ORGANISM(.+?)REFERENCE.+/$1/rs;
-	    $taxonInfo =~ s/\n/>/g;
-      $taxonInfo =~ s/\s+/_/g;
-	    $taxonInfo =~ s/\>\>/;/g;
-	    $taxonInfo =~ s/;(_|>)/>/g;
-	    $taxonInfo =~ s/_([A-Z][a-z])/$1/g;
+	    $taxonInfo =~ s/\n/@/;
+	    ($speciesName, $taxon) = split /\@/, $taxonInfo;
+	    $speciesName =~ s/\s+/_/g;
+	    $speciesName =~ s/_([A-Z][a-z])/$1/g;
+	    $taxon =~ s/\n/>/g;
+      $taxon =~ s/\s+/_/g;
+	    $taxon =~ s/\>\>/;/g;
+	    $taxon =~ s/;(_|>)/>/g;
+	    $taxon =~ s/_([A-Z][a-z])/$1/g;
 	    
-	    print TI "$accession\t$taxonInfo\n";
+	    print TI "$accession\t$speciesName\t$taxon\n";
 }
